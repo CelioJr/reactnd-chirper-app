304ecb6a71f3ffaf5edcd5b7498a05bd5e900188
Celio Junior <celiofjunior01@gmail.com>
2019-02-26 17:45:58 -0400

    up new changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+import NewTweet from './NewTweet'
+import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
+  componentDidMount(){
+    this.props.dispatch(handleInitialData())
+  }
+
   render() {
     return (
       <div>
-        Starter Code
+        <LoadingBar/>
+        {this.props.loading === true
+          ? null
+          // : <Dashboard/>
+          : <NewTweet/>
+        }
       </div>
     )
   }
 }
 
-export default App
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+export default connect(mapStateToProps)(App)
